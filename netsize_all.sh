8a8ac1b63a47b72fb590981ad054b86d4c7676e3
Fernandatenemaza2001 <95723069+Fernandatenemaza2001@users.noreply.github.com>
2021-12-11 20:48:07 -0500

    Add files via upload
    Parte 2 del ejercicio

---
+++ netsize_all.sh
@@ -0,0 +1,5 @@
+cd ../../
+cd Documents/deberbioinfor/unix/data/Saavedra2013
+for file in ../Saavedra2013/*.txt; do cat $file | wc -l; done
+for files in ../Saavedra2013/*.txt; do head -n 1 $files | tr -d " " | tr -d "\n" | wc -c; done
+
