c32b75aa8823e48b6ced218f3502d7192246256c
Fernandatenemaza2001 <95723069+Fernandatenemaza2001@users.noreply.github.com>
2021-12-11 22:50:08 -0500

    Update netsize.sh
    cd: comando usado para el cambio de directorio de trabajo

--- netsize.sh
+++ netsize.sh
@@ -1,8 +1,1 @@
 cd ../../
-cd Documents/deberbioinfor/unix/data
-cat ../data/Saavedra2013/n58.txt| wc -l
-echo numero de columnas
-head -n 1 ../data/Saavedra2013/n58.txt|tr -d ' '|tr -d '\n'| wc -c
-# head: Se emplea para ver la primera fila
-# wc: Se emplaea para contar el número de líneas, palabras y caracteres
-# cat: se emplaea para imprimir el contenido de los archivos
