45662223293527ee93f3819464b95b916ddbb7b0
Fernandatenemaza2001 <95723069+Fernandatenemaza2001@users.noreply.github.com>
2021-12-11 22:55:35 -0500

    Update netsize.sh
    head: Se emplea para ver la primera fila

--- netsize.sh
+++ netsize.sh
@@ -1,3 +1,5 @@
 cd ../../
 cd Documentos / deberbioinfor / unix / data
 cat ../data/Saavedra2013/n58.txt | wc -l
+echo numero de columnas
+head -n 1 ../data/Saavedra2013/n58.txt|tr -d ' '|tr -d '\n'| wc -c
