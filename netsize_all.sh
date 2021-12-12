98d2cea8dae89809185649dc2da15c0de7570b79
Fernandatenemaza2001 <95723069+Fernandatenemaza2001@users.noreply.github.com>
2021-12-11 22:59:51 -0500

    Update netsize_all.sh
    cd: cambio de directorio

--- netsize_all.sh
+++ netsize_all.sh
@@ -2,4 +2,3 @@ cd ../../
 cd Documents/deberbioinfor/unix/data/Saavedra2013
 for file in ../Saavedra2013/*.txt; do cat $file | wc -l; done
 for files in ../Saavedra2013/*.txt; do head -n 1 $files | tr -d " " | tr -d "\n" | wc -c; done
-
